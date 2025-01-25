61a2c6eb0025297167789170c11c4ba5ff9d30e8
Shelikedeknerd <ryota.kc.53@gmail.com>
2025-01-26 00:44:39 +0700

    Craete Project
    1. Design Structure of project
    2. finish coding in routes folder
    3. setting in main.go

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+	"github.com/dekbadnerd/restaurant-management/database"
+	"github.com/dekbadnerd/restaurant-management/middleware"
+	"github.com/dekbadnerd/restaurant-management/routes"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.AuthMiddleware())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
+}
